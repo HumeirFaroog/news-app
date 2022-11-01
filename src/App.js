4c53610e30209dc83a01f0cf0f8ed83bfab2c650
HumeirFaroog <humeira306faroog@gmail.com>
2022-11-01 22:58:11 +0300

    making Cards

--- src/App.js
+++ src/App.js
@@ -5,10 +5,16 @@ import NewsList from './Pages/NewsList'
 function App() {
   return (
     <div className="App">
-      <h1 className="title"> See The Latest News</h1>
-      <div className="navbar">
-        
-      </div>
+      {/* <h1 className="title"> See The Latest News</h1> */}
+      <nav className="navbar"> 
+        <ul>
+          <li><a href="/"> Fashion </a></li>
+          <li><a href="/"> Football </a></li>
+          <li><a href="/"> Health </a></li>
+          <li><a href="/"> Technology </a></li>
+          <li class="flt"><a class="active" href="#about">News App </a></li>
+        </ul>
+      </nav>
       <NewsList />
     </div>
   );
--- src/Pages/NewsItem.js
+++ src/Pages/NewsItem.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import './items.css'
+import { navBar } from './navBar'
 
 export const NewsItem = ({urlToImage, title, description, publishedAt, url }) => {
   return (
-    <div className="news-pp">
-        <div className="news-items">
+    <div className="cards">
+        <div className="card">
             <img className="news-img" src={urlToImage} alt={urlToImage} />
+            <div class="text">
             <h3>{title}</h3>
             <date>{publishedAt}</date>
-            <p>{description}</p>
+            <p class="content">{description}</p>
             <a href={url}>Source of Information</a>
+            </div>
             
         </div>
     </div>
