a70a038216d8e908a16af69d824bd34280bf2bff
HumeirFaroog <humeira306faroog@gmail.com>
2022-11-01 11:46:33 +0300

    secand

---
+++ src/Pages/navBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const navBar = () => {
+  return (
+    <div className='navBar-app'>
+        <a> Fashion</a>
+        <a> football</a>
+        <a> Fashion</a>
+        <a> Fashion</a>
+    </div>
+  )
+}
